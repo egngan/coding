1bec07007344e9f4400b898d38d525edfbb90a9e
Elizabeth Ngan <egngan@gmail.com>
2018-08-11 16:55:40 +1200

    Implemented collision logic for a single pole

--- python/pygame/tutorial/Flappy.py
+++ python/pygame/tutorial/Flappy.py
@@ -12,35 +12,30 @@ white = (255,255,255)
 red = (255,0,0)
 
 bird_height = 36
+bird_width = 52
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Bouncing Avian!')
+pygame.display.set_caption('Bouncing Avian')
 clock = pygame.time.Clock()
 
 birdImg = pygame.image.load('bird.png')
 
 class Pole:
     pole_width = 70
-    def __init__(self, x_pos, y_gap, h_gap):
+    def __init__(self, x_pos, gap_position, gap_height):
         self.x_pos = x_pos
-        self.y_gap = y_gap
-        self.h_gap = h_gap
+        self.gap_position = gap_position
+        self.gap_height = gap_height
 
-    def move(self,x_pos):
-        self.x_pos = x_pos
+    def move(self,x_delta):
+        self.x_pos += x_delta
         
-    def draw(self):
-        h_gap2 = self.h_gap*0.5
-        y1 = self.y_gap - h_gap2
-        y2 = self.y_gap + h_gap2
-        pygame.draw.rect(gameDisplay, black, [self.x_pos, y2 , self.pole_width, display_height - y2])
-        pygame.draw.rect(gameDisplay, black, [self.x_pos, 0 , self.pole_width, y1])
-
-def things(thingx, y_pass, thingw, h_pass, color):
-    y1 = y_pass - h_pass*0.5
-    y2 = y_pass + h_pass*0.5
-    pygame.draw.rect(gameDisplay, color, [thingx, y2 , thingw, display_height - y2])
-    pygame.draw.rect(gameDisplay, color, [thingx, 0 , thingw, y1])
+    def draw(self, pole_colour):
+        gap_height_half = self.gap_height*0.5
+        y1 = self.gap_position - gap_height_half
+        y2 = self.gap_position + gap_height_half
+        pygame.draw.rect(gameDisplay, pole_colour, [self.x_pos, y2 , self.pole_width, display_height - y2])
+        pygame.draw.rect(gameDisplay, pole_colour, [self.x_pos, 0 , self.pole_width, y1])
 
 def bird(x,y):
     gameDisplay.blit(birdImg,(x,y))
@@ -67,27 +62,22 @@ def crash():
     message_display('You Died :(')
     
 def game_loop():
-    x = (display_width * 0.25)
-    y = (display_height * 0.1)
+    bird_x = (display_width * 0.25)
+    bird_y = (display_height * 0.1)
 
     y_velocity = 0
     x_velocity = -2
-
-    #thing_startx = random.randrange(display_width*0.5, display_width)
-    pole_x = display_width*1.1
-    h_pass =  random.randrange(60, 150)
-    y_pass =  random.randrange(100, display_height)
-    #y_pass =  random.randrange(h_pass * 0.5, display_height - h_pass * 0.5)
-    thing_speed = 7
-    thing_width = 70
     gravity = 0.1
     y_acceleration = gravity
     lift = 0
+    pole_x_position = display_width*1.1
+    pole_gap_height = random.randrange(100, 150)
+    pole_gap_position = random.randrange(int(pole_gap_height * 0.5), int(display_height - pole_gap_height * 0.5))
 
-    pole = Pole(display_width*1.1,  random.randrange(int(h_pass * 0.5), int(display_height - h_pass * 0.5)), random.randrange(60, 150))
+    poles = [Pole(pole_x_position, pole_gap_position, pole_gap_height)]
+    pole_colour = black
 
     gameExit = False
-    firstTime = True
     while not gameExit:
 
         for event in pygame.event.get():
@@ -105,20 +95,41 @@ def game_loop():
                     lift = 0
         y_acceleration = gravity + lift
         y_velocity += y_acceleration
-        y += y_velocity
+        bird_y += y_velocity
         gameDisplay.fill(white)
         
-        pole.move(pole_x)
-        pole.draw()
+        for pole in poles: 
+            pole.move(x_velocity)
+            pole.draw(pole_colour)
         
         # # things(thingx, thingy, thingw, thingh, color)
         #things(thing_startx, y_pass, thing_width, h_pass, black)
         #thing_starty += thing_speed
-        bird(x,y)
-        if y > display_height - bird_height or y <= 0:
+        bird(bird_x,bird_y)
+
+        #bird will crash if it touched the top and bottom of the pole gap
+        pole_gap_height_half = pole_gap_height*0.5
+        gap_top = pole_gap_position - pole_gap_height_half
+        gap_bottom = pole_gap_position + pole_gap_height_half
+        avoid_hitting_top = bird_y > gap_top
+        avoid_hitting_bottom = (bird_y + bird_height) < gap_bottom
+        can_pass_through = avoid_hitting_top and avoid_hitting_bottom
+        if bird_y > display_height - bird_height or bird_y <= 0:
+            crash()
+        # the bird won't crash before and after pole
+        bird_front = bird_x + bird_width
+        pole_front = pole.x_pos
+        bird_back = bird_x
+        pole_back = pole.x_pos + pole.pole_width
+        if (bird_front >= pole_front) and (bird_back <= pole_back) and not can_pass_through:
             crash()
 
-        pole_x += x_velocity
+        if not can_pass_through:
+            pole_colour = red 
+        else:
+            pole_colour = black
+
+        #pole_x += x_velocity
 
         #if thing_startx > display_height:
             #thing_startx = 0 - thing_height
