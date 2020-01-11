915ad265c75e1e586eb680ddb4652cdde8f52092
Elizabeth Ngan <egngan@gmail.com>
2020-01-11 17:50:07 +1300

    Update Flappy.py
    Added Current Score

--- python/pygame/tutorial/Flappy.py
+++ python/pygame/tutorial/Flappy.py
@@ -21,6 +21,29 @@ clock = pygame.time.Clock()
 
 birdImg = pygame.image.load('bird.png')
 
+class CurrentScore:
+    def __init__(self):
+        #self.is_bird_passing_pole = False
+        self.current_score = 0
+
+    # def UpdateScore (self, pole_front, pole_back, bird_front, bird_back):
+    #     bird_in_pole = bird_front > pole_front and bird_back < pole_back
+    #     if self.is_bird_passing_pole and not bird_in_pole:
+    #         self.current_score += 1
+    #     self.is_bird_passing_pole = bird_in_pole
+
+    def UpdateScore (self, pole_front, pole_back, bird_front, bird_back, is_bird_passing_pole):
+        bird_in_pole = bird_front > pole_front and bird_back < pole_back
+        if is_bird_passing_pole and not bird_in_pole:
+            self.current_score += 1
+        return bird_in_pole
+
+    def Score (self):
+        return self.current_score
+
+# End of CurrentScore: --------------------------------------------------------------
+
+current_score = CurrentScore() 
 
 class DynamicPoles:
     def __init__(self, pole_count):
@@ -35,7 +58,7 @@ class DynamicPoles:
                 self.pole_collection.remove(pole)
         # possibly add pole if collection contains fewer than pole count
         
-        pole_gap_height = random.randrange(200, 450)
+        pole_gap_height = random.randrange(50, 450)
         pole_gap_position = random.randrange(
             int(pole_gap_height * 0.5), int(display_height - pole_gap_height * 0.5))
 
@@ -51,6 +74,8 @@ class DynamicPoles:
 
 class Pole:
     pole_width = 70
+    is_bird_passing_pole = False
+
     def __init__(self, x_pos, gap_position, gap_height):
         self.x_pos = x_pos
         self.gap_position = gap_position
@@ -85,22 +110,28 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf',115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-
-    pygame.display.update()
-
-    time.sleep(2)
-
-    game_loop()
-    
-    
+    text_position = ((display_width/2),(display_height/2))
+    display_text(text, 115, text_position)
 
 def crash():
     message_display('You Died :(')
-    
+    pygame.display.update()
+
+def display_text(text, font_size, text_position):
+    largeText = pygame.font.Font('freesansbold.ttf',font_size)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = text_position
+    gameDisplay.blit(TextSurf, TextRect)
+
+def display_score():
+    display_current_score()
+
+def display_current_score():
+    current_score_position_x = 50
+    current_score_position_y = display_height - 50
+    text_position = (current_score_position_x, current_score_position_y)
+    display_text(str(current_score.Score()), 30, text_position)
+
 def game_loop():
     bird_x = (display_width * 0.25)
     bird_y = (display_height * 0.1)
@@ -110,18 +141,13 @@ def game_loop():
     gravity = 0.05
     y_acceleration = gravity
     lift = 0
-    # pole_x_position = display_width*1.1
-    # pole_gap_height = random.randrange(100, 150)
-    # pole_gap_position = random.randrange(int(pole_gap_height * 0.5), int(display_height - pole_gap_height * 0.5))
-
-    # poles = [Pole(pole_x_position, pole_gap_position, pole_gap_height), Pole(pole_x_position + 100, pole_gap_position, pole_gap_height)]
     pole_colour = black
 
     poles = DynamicPoles(5)
 
     gameExit = False
     while not gameExit:
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -129,7 +155,7 @@ def game_loop():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-                    lift = -0.35
+                    lift = -0.2
 
 
             if event.type == pygame.KEYUP:
@@ -145,9 +171,6 @@ def game_loop():
             pole.move(x_velocity)
             pole.draw(pole_colour)
             
-            # # things(thingx, thingy, thingw, thingh, color)
-            #things(thing_startx, y_pass, thing_width, h_pass, black)
-            #thing_starty += thing_speed
             bird(bird_x,bird_y)
 
             #bird will crash if it touched the top and bottom of the pole gap
@@ -159,6 +182,7 @@ def game_loop():
             can_pass_through = avoid_hitting_top and avoid_hitting_bottom
             if bird_y > display_height - bird_height or bird_y <= 0:
                 crash()
+                gameExit = True
             # the bird won't crash before and after pole
             bird_front = bird_x + bird_width
             pole_front = pole.x_pos
@@ -166,33 +190,25 @@ def game_loop():
             pole_back = pole.x_pos + pole.pole_width
             if (bird_front >= pole_front) and (bird_back <= pole_back) and not can_pass_through:
                 crash()
+                gameExit = True
 
             if not can_pass_through:
                 pole_colour = red 
             else:
                 pole_colour = black
 
-            #pole_x += x_velocity
-
-            #if thing_startx > display_height:
-                #thing_startx = 0 - thing_height
-                #thing_startx = random.randrange(0,display_width)
-
-            # ####
-            # if y < thing_starty+thing_height:
-            #     print('y crossover',y,thing_starty)
-
-            #     if x > thing_startx and x < thing_startx + thing_width or x+bird_width > thing_startx and x + bird_width < thing_startx+thing_width:
-            #         print('x crossover')
-            #         crash()
-            # ####
-        
+            pole.is_bird_passing_pole = current_score.UpdateScore(pole_front, pole_back, bird_front, bird_back, pole.is_bird_passing_pole)
+            display_score()
+            
         pygame.display.update()
         clock.tick(60)
 
 
 game_loop()
+time.sleep(2)
+game_loop()
+time.sleep(2)
+game_loop()
+time.sleep(5)
 pygame.quit()
 quit()
-
-
